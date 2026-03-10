c623dddfaa9e31589b40768b051c263c969e48b8
Vit Bojanovsky <vit.bojanovsky@outlook.cz>
2026-03-10 09:19:53 +0100

    checkovani hesla

--- string.py
+++ string.py
@@ -1,3 +1,24 @@
-string = "bobek"
-for x in string:
-    print(x)
+heslo = "ahoj_svete"
+delka = len(heslo)
+male = False
+cisla = False
+specialni_znaky = False
+bezpecnost = 0
+if(delka>8):
+    bezpecnost = bezpecnost + 1
+if(heslo.islower()):
+    male = True
+else:
+    bezpecnost = bezpecnost + 1
+    male = False
+if(heslo.isalpha()):
+    cisla = False
+else:
+    cisla = True
+    bezpecnost = bezpecnost + 1
+if not any(char in "!@#$%^&*()" for char in heslo):
+    specialni_znaky = False
+else:
+    specialni_znaky = True
+    bezpecnost = bezpecnost + 1
+print(f"bezpecnostni skore = {bezpecnost}")
