007725be649decf983ca52fdf43af51ad34f77d6
Vit Bojanovsky <vit.bojanovsky@outlook.cz>
2026-03-18 13:03:30 +0100

    factorial

---
+++ factorial.py
@@ -0,0 +1,14 @@
+def absolutni_hodnota(n):
+    return abs(int(n))
+def factorial(x):
+    x = absolutni_hodnota(x)
+    factorial = 1
+    if(x == 0):
+        return factorial
+    for i in range(1,int(x)+1,1):
+        factorial = factorial*i
+    return factorial
+print("Zadejte cislo: ")
+cislo = int(input())
+cislo = factorial(cislo)
+print(f"factorial je {cislo}")
