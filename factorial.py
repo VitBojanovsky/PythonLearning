83a8b93a4b4c935285581420ec262d88521735aa
Vit Bojanovsky <vit.bojanovsky@outlook.cz>
2026-03-18 13:08:19 +0100

    upgrade

--- factorial.py
+++ factorial.py
@@ -1,3 +1,5 @@
+from math import factorial
+'''
 def absolutni_hodnota(n):
     return abs(int(n))
 def factorial(x):
@@ -8,7 +10,10 @@ def factorial(x):
     for i in range(1,int(x)+1,1):
         factorial = factorial*i
     return factorial
+    '''
+
 print("Zadejte cislo: ")
 cislo = int(input())
+#cislo = factorial(cislo)
 cislo = factorial(cislo)
 print(f"factorial je {cislo}")
