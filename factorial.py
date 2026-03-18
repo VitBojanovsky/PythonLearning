41941a86180400ce1163e7272cf89d556f13631a
Vit Bojanovsky <vit.bojanovsky@outlook.cz>
2026-03-18 13:25:47 +0100

    upgrade

--- factorial.py
+++ factorial.py
@@ -1,9 +1,10 @@
 from math import factorial
 import sys
 sys.set_int_max_str_digits(479001600)
-'''
+
 def absolutni_hodnota(n):
     return abs(int(n))
+'''
 def factorial(x):
     x = absolutni_hodnota(x)
     factorial = 1
@@ -16,6 +17,11 @@ def factorial(x):
 
 print("Zadejte cislo: ")
 cislo = int(input())
-#cislo = factorial(cislo)
+try:
+    cislo = int(cislo)
+except ValueError:
+    print("Zadejte platne cislo")
+    sys.exit()
+cislo = absolutni_hodnota(cislo)
 cislo = factorial(cislo)
 print(f"factorial je {cislo}")
