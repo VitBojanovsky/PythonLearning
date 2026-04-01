3ad1aab1a5476d4285d2d278720b1dcd02ccf67b
Vit Bojanovsky <vit.bojanovsky@outlook.cz>
2026-04-01 12:51:09 +0200

    .pop()

--- seznamkoncici0.py
+++ seznamkoncici0.py
@@ -1,13 +1,19 @@
 def zadanicisla(i) -> float:
-    print(f"Zadejte {i+1} cislo: ")
+    print(f"Zadejte {i} cislo: ")
     cislo = float(input())
     return cislo
-
 def vypis_seznam(seznam):
     for i in range(len(seznam)):
         print(seznam[i])
 
 seznam: list[float] = []
-for i in range(7):
+i = 0
+
+while True:
+    i = i + 1
     seznam.append(zadanicisla(i))
+    if(seznam[i-1] == 0):
+        i = 0
+        seznam.pop()
+        break
 vypis_seznam(seznam)
