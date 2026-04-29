f314c285c8287dcd99a1c99bbd23d0d767672f33
Vit Bojanovsky <vit.bojanovsky@outlook.cz>
2026-04-29 12:30:48 +0200

    prej je to spatne

--- 29.4..py
+++ 29.4..py
@@ -1,3 +1,4 @@
+from math import pow
 cele_cislo = int(input("Zadejte celé číslo: "))
 kladne = False
 if(cele_cislo>=0): 
@@ -9,9 +10,12 @@ if(cele_cislo % 2 == 0):
 if(kladne==False):
     cele_cislo = cele_cislo * -1
     kladne = True
+    print("Zaporne")
+else:
+    print("Kladne")
 milimetry = cele_cislo * 1000
 if(cele_cislo == 0):
     print("S 0 nepocitam")
 else:
-    print(f"{cele_cislo}m = {milimetry}mm a cislo je sude = {sude}")
+    print(f"{cele_cislo}m = {milimetry}mm a cislo je sude = {sude} obvod = {4*cele_cislo} obsah = {pow(cele_cislo,2)}")
 
