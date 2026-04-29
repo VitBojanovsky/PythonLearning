817fbfbed59b65c75e9a7ffb96ea197a87757e33
Vit Bojanovsky <vit.bojanovsky@outlook.cz>
2026-04-29 13:02:44 +0200

    sdcsdc

--- objem-zasobniku.py
+++ objem-zasobniku.py
@@ -1,5 +1,6 @@
 from math import pi
 from math import pow
+from time import sleep
 print("Zadejte polomer")
 while True:
     try:
@@ -11,9 +12,11 @@ while True:
 if(polomer<0):
     print("Cyba, zadali jste zaporne cislo")
     print("Prevadim na kladne cislo: ")
+    sleep(5)
     polomer = polomer * -1
 obsah = (pow(polomer,3)/3)*4 * pi
 voda_na_den = 5
 obsah_v_l=obsah*1000
 voda_pro_cloveka = obsah_v_l/voda_na_den
 print(f"Jednomu cloveku vyjde tento kulovy zasobnik na {voda_pro_cloveka:.2f} dni, nebo {(voda_pro_cloveka*24):.2f} hodin")
+exit(0)
